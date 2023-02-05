09aec55f4557d396642b3e383c57c9bb9dbf9c20
N3GR032 <110050157+N3GR032@users.noreply.github.com>
2023-02-04 19:38:27 -0500

    Add files via upload

--- peluqueriamim/settings.py
+++ peluqueriamim/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'peluqueriamim.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'peluqueriamim',
+        'NAME': 'peluqueriamim2',
         'USER': 'postgres',
         'PASSWORD': 'cadena12',
         'HOST':'127.0.0.1',
